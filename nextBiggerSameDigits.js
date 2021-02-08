584fdca8e55cd442ee4167625de81f15f2e6aa12
Richard Perrotta <perrottarichard@gmail.com>
2021-02-09 00:46:24 +0700

    workin on nextBigger

--- nextBiggerSameDigits.js
+++ nextBiggerSameDigits.js
@@ -20,11 +20,31 @@ function nextBigger(n) {
   }
   let first = bigger[0];
   let rest = bigger.slice(1);
-  // if (first === digits[0]) {
-  //   let answer = [first, ...rest].join("");
-  //   answer = +answer;
-  //   return answer;
-  // }
+  if (first === digits[0]) {
+    let indexToStartChanging;
+    let nextGreaterNum = 9;
+    let answer = [first, ...rest].join("");
+    for (let i = 0; i < digits.length; i++) {
+      if (digits[i] !== answer[i]) {
+        indexToStartChanging = i;
+        break;
+      }
+    }
+    console.log("rest1", rest);
+    let nextGreaterThan = digits[indexToStartChanging];
+    let numsGreaterThan = rest.filter((n) => n > nextGreaterThan);
+    numsGreaterThan = numsGreaterThan.map((n) => Number(n));
+    nextGreaterNum = Math.min(...numsGreaterThan);
+    let index = rest.map((n) => Number(n)).indexOf(nextGreaterNum);
+    let before = rest.slice(0, indexToStartChanging - 1);
+    let changed = rest.splice(index, 1);
+    console.log("rest2", rest);
+    console.log("before", before);
+    console.log("ch", changed);
+
+    // answer = +answer;
+    // return answer;
+  }
   let min;
   for (let x = rest.length - 1; x >= 0; x--) {
     if (rest[x] < rest[x - 1]) {
@@ -51,3 +71,4 @@ function nextBigger(n) {
 // console.log(nextBigger(144)); //414
 // console.log(nextBigger(111)); // -1
 console.log(nextBigger(9227652)); //9252267
+console.log(nextBigger(102530975551)); //102531055579
