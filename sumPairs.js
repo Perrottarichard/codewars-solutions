87e8c1eff8be5bf5fc501d70c2ffcd0ad032976a
Richard Perrotta <perrottarichard@gmail.com>
2021-02-15 19:05:57 +0700

    add solution

--- sumPairs.js
+++ sumPairs.js
@@ -1,21 +1,29 @@
 function sumPairs(ints, s) {
-  let ans = [];
-  for (let i = 0; i < ints.length; i++) {
-    for (let j = 1; j < ints.length; j++) {
-      if (ints[i] + ints[j] === s && j > i) {
-        ans.push([ints[i], ints[j], j]);
-      }
-      if (ans.length === 2) {
-        let smaller = ans.sort((a, b) => a[2] - b[2]);
-        return [smaller[0][0], smaller[0][1]];
-      }
-    }
-  }
-  if (ans.length === 0) {
-    return undefined;
-  }
-  if (ans.length === 1) {
-    return [ans[0][0], ans[0][1]];
+  //MY CODE:
+  // let ans = [];
+  // for (let i = 0; i < ints.length; i++) {
+  //   if (ans.length === 2) {
+  //     let smaller = ans.sort((a, b) => a[2] - b[2]);
+  //     return [smaller[0][0], smaller[0][1]];
+  //   }
+  //   for (let j = 1, z = ints[i], diff = s - z; j < ints.length; j++) {
+  //     if (ints[j] === diff && j > i) {
+  //       ans.push([z, ints[j], j]);
+  //       break;
+  //     }
+  //   }
+  // }
+  // if (ans.length === 0) {
+  //   return undefined;
+  // }
+  // if (ans.length === 1) {
+  //   return [ans[0][0], ans[0][1]];
+  // }
+  //OPTIMAL CODEWARS SOLUTION:
+  var seen = {};
+  for (var i = 0; i < ints.length; i++) {
+    if (seen[s - ints[i]]) return [s - ints[i], ints[i]];
+    seen[ints[i]] = true;
   }
 }
 console.log(sumPairs([1, 4, 8, 7, 3, 15], 8)); //[1,7]
