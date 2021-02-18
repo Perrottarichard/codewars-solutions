a65a88e5bed9a21566770bcbd7dbf95eafa790fe
Richard Perrotta <perrottarichard@gmail.com>
2021-02-18 15:14:26 +0700

    solved

--- consecutiveStrings.js
+++ consecutiveStrings.js
@@ -1,32 +1,24 @@
-//unscalable solution
-
 function longestConsec(strarr, k) {
-  if (strarr.length === 0 || k > strarr.length || k <= 0) {
-    return ''
-  } else if (k === 2) {
-    let l = ''
-    for (let i = 0; i < strarr.length - 1; i++) {
-      let x = strarr[i]
-      let y = strarr[i + 1]
-      if ((x.length + y.length) > l.length) {
-        l = x.concat(y)
-      }
-    }
-    return l
-  } else if (k === 3) {
-    let l = ''
-    for (let i = 0; i < strarr.length - 2; i++) {
-      let x = strarr[i]
-      let y = strarr[i + 1]
-      let z = strarr[i + 2]
-      if ((x.length + y.length + z.length) > l.length) {
-        l = x.concat(y).concat(z)
-      }
-    }
-    return l
+  if (k <= 0 || k > strarr.length) {
+    return "";
   }
-
+  let maxSum = 0;
+  let ans;
+  for (let i = 0; i <= strarr.length - k; i++) {
+    let s = strarr.slice(i, i + k);
+    let sum = 0;
+    for (let j = 0; j < s.length; j++) {
+      sum += s[j].length;
+    }
+    if (sum > maxSum) {
+      maxSum = sum;
+      ans = s.join("");
+    }
+  }
+  return ans;
 }
 
-console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3))
-console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2))
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3));
+console.log(
+  longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
+);
