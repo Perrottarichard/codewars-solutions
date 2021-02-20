b017c174c5a461dbda4855920ee3a854dc76addc
Richard Perrotta <perrottarichard@gmail.com>
2021-02-20 09:21:37 +0700

    solved

--- theObservedPIN.js
+++ theObservedPIN.js
@@ -10,6 +10,7 @@
 
 function getPINs(observed) {
   let combos = {
+    0: ["0", "8"],
     1: ["1", "2", "4"],
     2: ["2", "1", "3", "5"],
     3: ["3", "2", "6"],
@@ -21,49 +22,70 @@ function getPINs(observed) {
     9: ["9", "6", "8"],
   };
   let splitDigits = observed.split("");
-  let allDigitsToInclude = [];
-  splitDigits.forEach((element) => allDigitsToInclude.push(...combos[element]));
+  let poss = [];
+  splitDigits.forEach((element) => poss.push(combos[element]));
   let ans = [];
   if (splitDigits.length === 1) {
-    return allDigitsToInclude[0];
+    return poss[0];
   }
-  let data = {};
+
   if (splitDigits.length === 2) {
-    for (let i = 0; i < allDigitsToInclude.length; i++) {
-      // console.log("i", i);
-      for (let j = 0; j < allDigitsToInclude.length; j++) {
-        if (!data[allDigitsToInclude[i] + allDigitsToInclude[j]]) {
-          data[allDigitsToInclude[i] + allDigitsToInclude[j]] = true;
-        }
+    for (let i = 0; i < poss[0].length; i++) {
+      for (let j = 0; j < poss[1].length; j++) {
+        ans.push(poss[0][i] + poss[1][j]);
       }
     }
   }
   if (splitDigits.length === 3) {
-    for (let i = 0; i < allDigitsToInclude.length; i++) {
-      for (let j = 0; j < allDigitsToInclude.length; j++) {
-        for (let k = 0; k < allDigitsToInclude.length; k++) {
-          if (
-            !data[
-              allDigitsToInclude[i] +
-                allDigitsToInclude[j] +
-                allDigitsToInclude[k]
-            ] &&
-            combos[allDigitsToInclude[i]].includes(allDigitsToInclude[j]) &&
-            combos[allDigitsToInclude[i]].includes(allDigitsToInclude[k])
-          ) {
-            data[
-              allDigitsToInclude[i] +
-                allDigitsToInclude[j] +
-                allDigitsToInclude[k]
-            ] = true;
+    for (let i = 0; i < poss[0].length; i++) {
+      for (let j = 0; j < poss[1].length; j++) {
+        for (let k = 0; k < poss[2].length; k++) {
+          ans.push(poss[0][i] + poss[1][j] + poss[2][k]);
+        }
+      }
+    }
+  }
+  if (splitDigits.length === 4) {
+    for (let i = 0; i < poss[0].length; i++) {
+      for (let j = 0; j < poss[1].length; j++) {
+        for (let k = 0; k < poss[2].length; k++) {
+          for (let x = 0; x < poss[3].length; x++) {
+            ans.push(poss[0][i] + poss[1][j] + poss[2][k] + poss[3][x]);
           }
         }
       }
     }
   }
-
-  console.log(data);
+  if (splitDigits.length === 8) {
+    for (let i = 0; i < poss[0].length; i++) {
+      for (let j = 0; j < poss[1].length; j++) {
+        for (let k = 0; k < poss[2].length; k++) {
+          for (let x = 0; x < poss[3].length; x++) {
+            for (let r = 0; r < poss[4].length; r++) {
+              for (let f = 0; f < poss[5].length; f++) {
+                for (let a = 0; a < poss[6].length; a++) {
+                  for (let b = 0; b < poss[7].length; b++) {
+                    ans.push(
+                      poss[0][i] +
+                        poss[1][j] +
+                        poss[2][k] +
+                        poss[3][x] +
+                        poss[4][r] +
+                        poss[5][f] +
+                        poss[6][a] +
+                        poss[7][b]
+                    );
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return ans;
 }
-// console.log(getPINs("8")); //["5", "7", "8", "9", "0"]
-// console.log(getPINs("11")); //["11", "22", "44", "12", "21", "14", "41", "24", "42"]
+console.log(getPINs("8")); //["5", "7", "8", "9", "0"]
+console.log(getPINs("11")); //["11", "22", "44", "12", "21", "14", "41", "24", "42"]
 console.log(getPINs("369")); //["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"]
